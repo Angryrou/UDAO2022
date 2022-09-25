2c4d210954dd9d0c142c8fc68a5d9b8befc76746
QiFAN-lix <zdh1204fanqiqi@163.com>
2022-09-25 14:24:40 +0200

    add EVO with code, example

--- optimization/moo/generic_moo.py
+++ optimization/moo/generic_moo.py
@@ -12,17 +12,21 @@ import utils.optimization.moo_utils as moo_ut
 class GenericMOO:
 
     def __init__(self, moo_algo, solver, obj_names, obj_funcs, opt_types, const_funcs, const_types, var_types, var_bounds, add_confs):
-        assert moo_algo in ["weighted_sum", "progressive_frontier", "evolutionary", "mobo", "nnc"]
-        self.moo_algo = moo_algo
-        self.solver = solver
-        self.obj_names = obj_names
-        self.obj_funcs = obj_funcs
-        self.opt_types = opt_types
-        self.const_funcs = const_funcs
-        self.const_types = const_types
-        self.var_types = var_types
-        self.var_bounds = var_bounds
-        self.add_confs = add_confs
+        # common input paramters for MOO problems
+        self.obj_names = obj_names  # objective names
+        self.obj_funcs = obj_funcs  # objective functions
+        self.opt_types = opt_types  # optimization types (minimization/maximization)
+        self.const_funcs = const_funcs  # constraint functions
+        self.const_types = const_types  # constraint types ("<=" or "<", e.g. g1(x1, x2, ...) - c <= 0)
+        self.var_types = var_types  # variable types (float, integer, binary)
+        self.var_bounds = var_bounds  # lower and upper bounds of variables
+
+        # specify MOO algorithm, its solver and the specific input parameters the choice of MOO algorithm and its solver
+        assert moo_algo in ["weighted_sum", "progressive_frontier", "evolutionary", "mobo",
+                            "normalized_normal_constraint"]
+        self.moo_algo = moo_algo  # the name of MOO algorithms
+        self.solver = solver  # the name of solvers
+        self.add_confs = add_confs  # the parameters required by the specified MOO algorithm and solver
 
     def solve(self):
         if self.moo_algo == "weighted_sum":
@@ -48,7 +52,7 @@ class GenericMOO:
 
         elif self.moo_algo == "mobo":
             pass
-        elif self.moo_algo == "nnc":
+        elif self.moo_algo == "normalized_normal_constraint":
             pass
         else:
             raise NotImplementedError
