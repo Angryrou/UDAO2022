7a51f3861f7bd7cf017cf78113d539a7643d5c7c
angryrou <chenghao@cs.umass.edu>
2022-10-12 00:39:28 +0200

    rename

--- examples/optimization/ws/heuristic_closed_form/main.py
+++ examples/optimization/ws/heuristic_closed_form/main.py
@@ -6,7 +6,7 @@
 
 from optimization.moo.generic_moo import GenericMOO
 from utils.optimization.configs_parser import ConfigsParser
-import utils.optimization.functions_def as func_def
+from utils.optimization.pre_defined_funtions import HCF
 import utils.optimization.moo_utils as moo_ut
 
 HELP = """
@@ -15,12 +15,14 @@ Format: python ws_hcf.py -c <config> -h
 Example:
     python examples/optimization/ws/heuristic_closed_form/ws_hcf.py -c examples/optimization/ws/heuristic_closed_form/hcf_configs_grid_search.json
 """
+
 # get input parameters
 moo_algo, solver, var_types, var_ranges, obj_names, opt_types, const_types, add_params = ConfigsParser().parse_details()
 # problem setup
 moo = GenericMOO()
-moo.problem_setup(obj_names=obj_names, obj_funcs=[func_def.obj_func1, func_def.obj_func2], opt_types=opt_types,
-                  const_funcs=[func_def.const_func1, func_def.const_func2], const_types=const_types, var_types=var_types, var_ranges=var_ranges)
+moo.problem_setup(obj_names=obj_names, obj_funcs=[HCF.obj_func1, HCF.obj_func2], opt_types=opt_types,
+                  const_funcs=[HCF.const_func1, HCF.const_func2], const_types=const_types,
+                  var_types=var_types, var_ranges=var_ranges)
 # solve MOO problem
 po_objs, po_vars = moo.solve(moo_algo, solver, add_params)
 
