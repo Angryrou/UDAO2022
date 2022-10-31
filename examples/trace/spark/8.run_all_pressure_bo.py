94ea815b76d8e83306b10bb8110fba03eeff3c1d
angryrou <chenghao@cs.umass.edu>
2022-10-31 04:32:38 +0100

    Update 8.run_all_pressure_bo.py

--- examples/trace/spark/8.run_all_pressure_bo.py
+++ examples/trace/spark/8.run_all_pressure_bo.py
@@ -4,16 +4,19 @@
 #
 # Created at 10/30/22
 
-import pandas as pd
-import numpy as np
-import argparse, os
-import time
-from multiprocessing import Pool, Manager
+import argparse, os, time
+import random
+import threading
 
+from multiprocessing import Pool, Manager
+from multiprocessing.managers import ValueProxy
+
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-from trace.collect.framework import QueryQueue, submit, error_handler
+from trace.collect.framework import QueryQueue, error_handler, SparkCollect
 from trace.collect.sampler import BOSampler
+from trace.parser.spark import get_cloud_cost
 from utils.common import BenchmarkUtils, PickleUtils
 from utils.data.configurations import SparkKnobs, KnobUtils
 from utils.data.feature import NmonUtils
@@ -26,7 +29,7 @@ class Args():
         self.parser.add_argument("-b", "--benchmark", type=str, default="TPCH")
         self.parser.add_argument("-k", "--knob-meta-file", type=str, default="resources/knob-meta/spark.json")
         self.parser.add_argument("-s", "--seed", type=int, default=42)
-        self.parser.add_argument("--script-header", type=str, default="resources/scripts/tpch-lhs")
+        self.parser.add_argument("-q", "--query-header", type=str, default="resources/tpch-kit/spark-sqls")
         self.parser.add_argument("--out-header", type=str, default="examples/trace/spark/8.run_all_pressure_bo")
         self.parser.add_argument("--cache-header", type=str, default="examples/trace/spark/cache")
         self.parser.add_argument("--remote-header", type=str, default="~/chenghao")
@@ -40,13 +43,13 @@ class Args():
         self.parser.add_argument("--debug", type=int, default=0)
         self.parser.add_argument("--sgd-lr", type=float, default=1)
         self.parser.add_argument("--sgd-epochs", type=int, default=80)
-
+        self.parser.add_argument("--if-aqe", type=int, default=0)
 
     def parse(self):
         return self.parser.parse_args()
 
 
-def find_next_conf(tid, bo_sampler, observed, knob_signs, bo_trials, target_obj_id, lr, epochs):
+def find_next_sample(tid, observed, knob_signs, bo_trials, target_obj_id, lr, epochs):
     X, Y = observed["X"], observed["Y"]
     # add the recommended knob values
 
@@ -76,10 +79,66 @@ def find_next_conf(tid, bo_sampler, observed, knob_signs, bo_trials, target_obj_
         print(f"{tid}: trial {bo_trials} recommended {N_CANDS_PER_TRIAL} observed configuration, skip.")
 
 
-def extract(qq, templates, knob_signs_dict, i):
+def extract(qq, templates, i, next_sample_dict):
     tiid, qid = qq.index_to_tid_and_qid(i)
     tid = templates[tiid]
-    knob_sign = knob_signs_dict[tid]
+    next_sample = next_sample_dict[tid]
+    return tid, qid, next_sample
+
+
+def submit(
+        lock: threading.RLock,
+        current_cores: ValueProxy,
+        cores: int,
+        tid: str,
+        qid: str,
+        conf_dict: dict,
+        debug: bool,
+        out_header: str,
+        new_sample: np.ndarray,
+        target_obj_id: int,
+        if_aqe: bool
+):
+    header = f"{out_header}/{tid}"
+    os.makedirs(header, exist_ok=True)
+    os.makedirs(f"{header}/log", exist_ok=True)
+
+    file_name = spark_collect.save_one_script(tid, qid, conf_dict, header, if_aqe)
+    log_file = f"{header}/log/q{tid}-{qid}.log"
+
+    print(f"Thread {tid}-{qid}: start running")
+    start = time.time()
+    if debug:
+        time.sleep(random.randint(1, 5))
+    else:
+        os.system(f"bash {header}/{file_name} > {log_file} 2>&1")
+    lat = time.time() - start
+    with lock:
+        current_cores.value -= cores
+        print(f"Thread {tid}-{qid}: finish running, takes {lat}s, current_cores={current_cores.value}")
+
+    cost = get_cloud_cost(
+        lat=lat,
+        mem=int(conf_dict["spark.executor.memory"][:-1]),
+        cores=int(conf_dict["spark.executor.cores"]),
+        nexec=int(conf_dict["spark.executor.instances"])
+    )
+    X, Y = observed_dict[tid]["X"], observed_dict[tid]["Y"]
+    new_objs = np.array([lat, cost])
+    new_Y = objs_scaler_dict[tid].transform(new_objs)
+
+    observed_dict[tid] = {
+        "X": np.vstack([X, new_sample]),
+        "Y": np.vstack([Y, new_Y])
+    }
+
+    next_conf, new_observed, bo_trials = find_next_sample(
+        tid, observed, knob_signs, bo_trials=0, target_obj_id=target_obj_id,
+        lr=sgd_lr, epochs=sgd_epochs
+    )
+    observed_dict[tid] = new_observed
+    bo_trials_dict[tid] = bo_trials
+    next_sample_dict[tid] = next_conf
 
 
 if __name__ == '__main__':
@@ -88,7 +147,6 @@ if __name__ == '__main__':
 
     benchmark = args.benchmark
     seed = args.seed
-    script_header = args.script_header
     out_header = args.out_header
     cache_header = args.cache_header
     remote_header = args.remote_header
@@ -99,10 +157,13 @@ if __name__ == '__main__':
     cluster_cores = args.cluster_cores
     counts = args.counts
     freq = args.freq
+    query_header = args.query_header
     workers = BenchmarkUtils.get_workers(benchmark)
     debug = False if args.debug == 0 else True
-    templates = BenchmarkUtils.get(benchmark)
     sgd_lr, sgd_epochs = args.sgd_lr, args.sgd_epochs
+    if_aqe = False if args.if_aqe == 0 else True
+
+    templates = BenchmarkUtils.get(benchmark)
 
     log_header = f"{out_header}/log"
     nmon_header = f"{out_header}/nmon"
@@ -137,7 +198,7 @@ if __name__ == '__main__':
     objs_df_dict = PickleUtils.load(cache_header, f"lhs_{n_templates}x{qpt_lhs}_objs.pkl")
     observed_dict = {}
     objs_scaler_dict = {}
-    next_conf_dict = {}
+    next_sample_dict = {}
     knob_signs_dict = {}
     bo_trials_dict = {}
 
@@ -161,49 +222,71 @@ if __name__ == '__main__':
         knob_signs = knob_df.index.to_list()
         knob_signs_dict[tid] = knob_signs
         objs_scaler_dict[tid] = scaler
-        next_conf, new_observed, bo_trials = find_next_conf(tid, bo_sampler, observed, knob_signs,
-                                                            bo_trials=0, target_obj_id=0,
-                                                            lr=sgd_lr, epochs=sgd_epochs)
+        next_conf, new_observed, bo_trials = find_next_sample(
+            tid, observed, knob_signs, bo_trials=0, target_obj_id=0,
+            lr=sgd_lr, epochs=sgd_epochs
+        )
         observed_dict[tid] = new_observed
         bo_trials_dict[tid] = bo_trials
-        next_conf_dict[tid] = next_conf
-        print(f"{tid}: {next_conf}, {bo_trials}")
+        next_sample_dict[tid] = next_conf
+        print(f"{tid}, next_conf: {next_conf} at bo_trial {bo_trials}")
 
-    print(f"tid, next_conf, bo_trials")
-    for tid in templates:
-        print(f"{tid}, {next_conf_dict[tid]}, {bo_trials_dict[tid]}")
+    if debug:
+        print(f"tid, next_conf, bo_trials")
+        for tid in templates:
+            print(f"{tid}, {next_sample_dict[tid]}, {bo_trials_dict[tid]}")
 
     if not debug:
         os.system(nmon_reset)
         os.system(nmon_start)
 
-    # total_queries = n_templates * qpt_total
-    # m = Manager()
-    # current_cores = m.Value("i", 0)
-    # lock = m.RLock()
-    #
-    # submit_index = n_templates * qpt_lhs
-    # pool = Pool(processes=n_processes)
-    # while submit_index < total_queries:
-    #     tid, qid, knob_sign, cores = extract(qq, templates, next_conf_dict, submit_index)
-    #     with lock:
-    #         if cores + current_cores.value < cluster_cores:
-    #             current_cores.value += cores
-    #             if_submit = True
-    #             print(f"Main Process: submit {tid}-{qid}, current_cores = {current_cores.value}")
-    #         else:
-    #             if_submit = False
-    #     if if_submit:
-    #         pool.apply_async(func=submit,
-    #                          args=(lock, current_cores, cores, tid, qid, knob_sign, debug, script_header, log_header),
-    #                          error_callback=error_handler)
-    #         submit_index += 1
-    #
-    #     time.sleep(1)
-    #
-    # pool.close()
-    # pool.join()
-    #
-    # if not debug:
-    #     os.system(nmon_stop)
-    #     os.system(nmon_agg)
+    total_queries = n_templates * qpt_total
+    m = Manager()
+    current_cores = m.Value("i", 0)
+    lock = m.RLock()
+
+    spark_collect = SparkCollect(
+        benchmark=benchmark,
+        scale_factor=100,
+        spark_knobs=spark_knobs,
+        query_header=query_header,
+        seed=seed
+    )
+
+    submit_index = n_templates * qpt_lhs
+    pool = Pool(processes=n_processes)
+    submitted = 0
+    target_obj_id = 0
+    while submit_index < total_queries:
+        tid, qid, next_sample = extract(qq, templates, submit_index, next_sample_dict)
+        knob_sign = KnobUtils.knob_denormalize(next_sample, knobs)
+        knob_dict = {k.name: v for k, v in zip(knobs, KnobUtils.sign2knobs(knob_sign, knobs))}
+        conf_dict = spark_knobs.knobs2conf(knob_dict)
+        cores = int(conf_dict["spark.executor.cores"]) * (int(conf_dict["spark.executor.instances"]) + 1)
+        mem = int(conf_dict["spark.executor.memory"][:-1]) * (int(conf_dict["spark.executor.instances"]) + 1)
+
+        with lock:
+            if cores + current_cores.value < cluster_cores:
+                current_cores.value += cores
+                if_submit = True
+                print(f"Main Process: submit {tid}-{qid}, current_cores = {current_cores.value}")
+            else:
+                if_submit = False
+        if if_submit:
+            pool.apply_async(func=submit,
+                             args=(lock, current_cores, cores, tid, qid, conf_dict, debug,
+                                   out_header, next_sample, target_obj_id, if_aqe),
+                             error_callback=error_handler)
+            submit_index += 1
+
+        time.sleep(1)
+        submitted += 1
+        if submitted >= qpt_bo * n_templates:
+            target_obj_id = 1
+
+    pool.close()
+    pool.join()
+
+    if not debug:
+        os.system(nmon_stop)
+        os.system(nmon_agg)
