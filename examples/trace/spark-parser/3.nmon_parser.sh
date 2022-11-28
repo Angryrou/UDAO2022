0d4191759387465cb0d7ff83db029217523243ce
Chenghao Lyu <chenghao@cs.umass.edu>
2022-11-28 18:50:01 +0100

    Update 3.nmon_parser.sh

--- examples/trace/spark-parser/3.nmon_parser.sh
+++ examples/trace/spark-parser/3.nmon_parser.sh
@@ -3,15 +3,17 @@ $header=$2
 
 if [[ $input == "hex1" ]]
 then
-  workers = ("node2", "node3", "node4", "node5", "node6")
+  workers=("node2", "node3", "node4", "node5", "node6")
 elif [[ $input == "hex2" ]]
 then
-  workers = ("node7", "node8", "node9", "node10", "node11")
+  workers=("node7", "node8", "node9", "node10", "node11")
 else
   echo "unsupported input $input"
 fi
 
 for worker in workers
 do
+  echo "start working on ${worker}.nmon"
   pyNmonAnalyzer -i $header/${worker}.nmon -o nmon_extracted/${worker}
+  echo "finish working on ${worker}.nmon"
 done
