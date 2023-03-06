a2d42433bd59e0bec8c5a16c0dcee0e4ed9ac22a
QiFAN-lix <zdh1204fanqiqi@163.com>
2023-03-06 13:10:30 +0100

    add necessary comments

--- examples/optimization/performance.py
+++ examples/optimization/performance.py
@@ -6,7 +6,7 @@
 #
 # Author(s): Qi FAN <qi dot fan at polytechnique dot edu>
 #
-# Description: TODO
+# Description: Performance metric of MOO
 #
 # Created at 06/03/2023
 import torch as th
@@ -22,5 +22,7 @@ def dominated_space(pareto_set, ref_point):
 if __name__ == '__main__':
     pareto_set = np.array([[1.0, 10.0], [4.0, 8.0], [6.0, 2.0]])
     ref_point = np.array([10.0, 10.0])
+    # why times -1? --> by default, BoTorch solves a maximization problem.
+    # The following example is based on the Pareto set of minimization optimization problem, i.e. minimize all objectives
     dominated_volume = dominated_space(-1 * th.from_numpy(pareto_set), -1 * th.from_numpy(ref_point))
     print(dominated_volume)
