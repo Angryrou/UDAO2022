10c0a19b650bc8bd638af268241002833c52685d
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-10-09 17:24:44 +0200

    rename to UdaoDataset - slim requirements

--- udao/udao/datasets/dataset.py
+++ udao/udao/datasets/dataset.py
@@ -1,8 +1,6 @@
-import numpy as np
 from datasets import Dataset
 
 
-class UDAODataset(Dataset):
-    def __init__(self, array: np.ndarray) -> None:
+class UdaoDataset(Dataset):
+    def __init__(self) -> None:
         """UDAO dataset class"""
-        self.array: np.ndarray = array
