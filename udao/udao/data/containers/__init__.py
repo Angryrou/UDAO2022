3ad3b5b0cfe0bd2471e2e60ee18466f8530a9d56
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-10-23 09:31:12 +0200

    missing init for containers

---
+++ udao/udao/data/containers/__init__.py
@@ -0,0 +1,9 @@
+from .base_container import BaseContainer
+from .dataframe_container import DataFrameContainer
+from .query_structure_container import QueryStructureContainer
+
+__all__ = [
+    "BaseContainer",
+    "DataFrameContainer",
+    "QueryStructureContainer",
+]
