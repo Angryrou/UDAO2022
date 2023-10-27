1465bf8a2cdfb8afe6fb235cca33063a77afb187
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-10-27 10:57:27 +0200

    add test for diff hidden vs output

--- udao/udao/model/tests/embedders/test_graph_transformer.py
+++ udao/udao/model/tests/embedders/test_graph_transformer.py
@@ -1,6 +1,7 @@
 from typing import cast
 
 import dgl
+import pytest
 import torch as th
 
 from ...embedders.graph_transformer import GraphTransformer, GraphTransformerParams
@@ -12,7 +13,7 @@ def test_graph_transformer_initialization() -> None:
     for attention_layer in ["QF", "RAAL", "GTN"]:
         net_params = GraphTransformerParams(
             input_size=3,
-            output_size=2,
+            output_size=6,
             op_groups=["ch1_type", "ch1_cbo"],
             type_embedding_dim=3,
             embedding_normalizer="BN",
@@ -32,6 +33,25 @@ def test_graph_transformer_initialization() -> None:
 
         model = GraphTransformer(net_params)
         assert isinstance(model, GraphTransformer)
+
+
+def test_graph_transformer_initialization_raises_error() -> None:
+    with pytest.raises(ValueError):
+        net_params = GraphTransformerParams(
+            input_size=3,
+            output_size=3,
+            op_groups=["ch1_type", "ch1_cbo"],
+            type_embedding_dim=3,
+            embedding_normalizer="BN",
+            n_op_types=4,
+            pos_encoding_dim=5,
+            n_layers=3,
+            n_heads=2,
+            hidden_dim=6,
+            readout="sum",
+            dropout=0.1,
+        )
+        GraphTransformer(net_params)
 
 
 def test_graph_transformer_forward() -> None:
