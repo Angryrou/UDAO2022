5a6c84aa4bdd1a1d46ee63ca3268b025288f765b
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-11-06 13:49:57 +0100

    add tests for custom metrics and fix

--- udao/udao/model/utils/metrics.py
+++ udao/udao/model/utils/metrics.py
@@ -9,23 +9,24 @@ EPS = 1e-7
 class ErrorPercentiles(Metric):
     def __init__(
         self,
-        quantiles: List[float] = [0.5, 0.9, 0.95, 0.99],
+        percentiles: List[float] = [50, 90, 95, 99],
         dist_sync_on_step: bool = False,
     ) -> None:
         super().__init__(dist_sync_on_step=dist_sync_on_step)
-        self.quantiles = quantiles
-        self.add_state("error_rate", default=th.empty((0, 1)), dist_reduce_fx=None)
+        self.quantiles = th.tensor(percentiles) / 100
+        self.add_state("error_rate", default=th.empty((0)), dist_reduce_fx=None)
 
     def update(self, input: th.Tensor, target: th.Tensor) -> None:
-        error_rate = th.abs(input - target) / (target + EPS)
-        self.error_rate.cat(error_rate)  # type: ignore
+        new_error_rate = th.abs(input - target) / (th.clip(target, min=EPS))
+        self.error_rate = th.cat([self.error_rate, new_error_rate])  # type: ignore
 
     def compute(self) -> Dict[str, th.Tensor]:
         error_quantiles = th.quantile(self.error_rate, self.quantiles, dim=0)  # type: ignore
         return {
             "mean": self.error_rate.mean(),  # type: ignore
             **{
-                f"q_{q * 100}": error_quantiles[i] for i, q in enumerate(self.quantiles)
+                f"q_{int(q * 100)}": error_quantiles[i]
+                for i, q in enumerate(self.quantiles)
             },
         }
 
@@ -33,26 +34,26 @@ class ErrorPercentiles(Metric):
 class QErrorPercentiles(Metric):
     def __init__(
         self,
-        quantiles: List[float] = [0.5, 0.9, 0.95, 0.99],
+        percentiles: List[int] = [50, 90, 95, 99],
         dist_sync_on_step: bool = False,
     ) -> None:
         super().__init__(dist_sync_on_step=dist_sync_on_step)
-        self.quantiles = quantiles
-        self.add_state("q_error", default=th.empty((0, 1)), dist_reduce_fx=None)
+        self.quantiles = th.tensor(percentiles) / 100
+        self.add_state("q_error", default=th.empty((0)), dist_reduce_fx=None)
 
     def update(self, input: th.Tensor, target: th.Tensor) -> None:
-        q_error = th.maximum(
-            (target + EPS) / (input + EPS),
-            (input + EPS) / (target + EPS),
+        new_q_error = th.maximum(
+            th.clip(target, EPS) / th.clip(input, EPS),
+            th.clip(input, EPS) / th.clip(target, EPS),
         )
-        self.q_error.cat(q_error)  # type: ignore
+        self.q_error = th.cat([self.q_error, new_q_error])  # type: ignore
 
     def compute(self) -> Dict[str, th.Tensor]:
         q_error_quantiles = th.quantile(self.q_error, self.quantiles, dim=0)  # type: ignore
         return {
             "mean": self.q_error.mean(),  # type: ignore
             **{
-                f"q_{q * 100}": q_error_quantiles[i]
+                f"q_{int(q * 100)}": q_error_quantiles[i]
                 for i, q in enumerate(self.quantiles)
             },
         }
