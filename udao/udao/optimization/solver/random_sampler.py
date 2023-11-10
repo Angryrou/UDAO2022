cfc05c426bbace0db14f6b4052d0d9f4696ba18e
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-11-10 09:30:45 +0100

    move logic to separate method

--- udao/udao/optimization/solver/random_sampler.py
+++ udao/udao/optimization/solver/random_sampler.py
@@ -16,49 +16,50 @@ class RandomSampler(BaseSolver):
     @dataclass
     class Params:
         n_samples_per_param: int
+        "the number of samples per variable"
         seed: Optional[int] = None
+        "random seed for generatino of samples"
 
     def __init__(self, params: Params) -> None:
         """
-        :param rs_params: int, the number of samples per variable
+        :param params: RandomSampler.Params
         """
         super().__init__()
         self.n_samples_per_param = params.n_samples_per_param
         self.seed = params.seed
 
+    def _process_variable(self, var: Variable) -> np.ndarray:
+        """Generate samples of a variable"""
+        if isinstance(var, FloatVariable):
+            return np.random.uniform(var.lower, var.upper, self.n_samples_per_param)
+        elif isinstance(var, IntegerVariable):
+            return np.random.randint(
+                var.lower, var.upper + 1, size=self.n_samples_per_param
+            )
+        elif isinstance(var, EnumVariable):
+            inds = np.random.randint(0, len(var.values), size=self.n_samples_per_param)
+            return np.array(var.values)[inds]
+        else:
+            raise NotImplementedError(
+                f"ERROR: variable type {type(var)} is not supported!"
+            )
+
     def _get_input(self, variables: List[Variable]) -> np.ndarray:
         """
         generate samples of variables
-        :param var_ranges: array (n_vars,),
+
+        Parameters:
+        -----------
+        variables: List[Variable],
             lower and upper var_ranges of variables(non-ENUM),
             and values of ENUM variables
-        :param var_types: list,
-            type of each variable
-        :return: array,
+        Returns:
+        np.ndarray,
             variables (n_samples * n_vars)
         """
         n_vars = len(variables)
         x = np.zeros([self.n_samples_per_param, n_vars])
         np.random.seed(self.seed)
         for i, var in enumerate(variables):
-            # randomly sample n_samples within the range
-            if isinstance(var, FloatVariable):
-                x[:, i] = np.random.uniform(
-                    var.lower, var.upper, self.n_samples_per_param
-                )
-            elif isinstance(var, IntegerVariable):
-                x[:, i] = np.random.randint(
-                    var.lower, var.upper + 1, size=self.n_samples_per_param
-                )
-            elif isinstance(var, EnumVariable):
-                inds = np.random.randint(
-                    0, len(var.values), size=self.n_samples_per_param
-                )
-                x[:, i] = np.array(var.values)[inds]
-            # TODO: extend to a matrix variable for the assignment problem in the future
-            else:
-                raise Exception(
-                    "Random-Sampler solver does not"
-                    f" support variable type {type(var)}!"
-                )
+            x[:, i] = self._process_variable(var)
         return x
