87f1780cc3fb331e24c590f62cfcb8811caf9364
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-11-30 11:45:06 +0100

    mogd inherits BaseSolver

--- udao/udao/optimization/concepts/constraint.py
+++ udao/udao/optimization/concepts/constraint.py
@@ -1,12 +1,36 @@
 from dataclasses import dataclass
-from typing import Callable, Literal, Union
+from typing import Any, Callable, Literal, Union
 
-import numpy as np
+import torch as th
+
+from ...data.handler.data_processor import DataProcessor
+from .utils import ModelComponent
 
 ConstraintType = Union[Literal["=="], Literal["<="], Literal[">="]]
 
 
 @dataclass
 class Constraint:
-    type: ConstraintType
-    function: Callable[..., np.ndarray]
+    lower: float
+    upper: float
+    function: Callable[..., th.Tensor]
+    stress: float = 0.0
+
+    def __call__(self, *args: Any, **kwargs: Any) -> th.Tensor:
+        return self.function(*args, **kwargs)
+
+
+class ModelConstraint(ModelComponent, Constraint):
+    def __init__(
+        self,
+        lower: float,
+        upper: float,
+        data_processor: DataProcessor,
+        model: th.nn.Module,
+    ) -> None:
+        ModelComponent.__init__(self, data_processor, model)
+
+        def function(*args: Any, **kwargs: Any) -> th.Tensor:
+            return self.apply_model(*args, **kwargs)
+
+        Constraint.__init__(self, lower, upper, function)
--- udao/udao/optimization/concepts/objective.py
+++ udao/udao/optimization/concepts/objective.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
-from typing import Callable, Literal, Optional, Union
+from typing import Any, Callable, Literal, Optional, Union
 
 import torch as th
 
+from ...data.handler.data_processor import DataProcessor
 from ...utils.interfaces import VarTypes
+from .utils import ModelComponent
 
 ObjectiveDirection = Union[Literal["MIN"], Literal["MAX"]]
 
@@ -22,3 +24,23 @@ class Objective:
             return 1
         else:
             return -1
+
+    def __call__(self, *args: Any, **kwargs: Any) -> th.Tensor:
+        return self.function(*args, **kwargs)
+
+
+class ModelObjective(ModelComponent, Objective):
+    def __init__(
+        self,
+        name: str,
+        direction_type: ObjectiveDirection,
+        data_processor: DataProcessor,
+        model: th.nn.Module,
+        type: Optional[VarTypes] = None,
+    ) -> None:
+        ModelComponent.__init__(self, data_processor, model)
+
+        def function(*args: Any, **kwargs: Any) -> th.Tensor:
+            return self.apply_model(*args, **kwargs)
+
+        Objective.__init__(self, name, direction_type, function, type)
