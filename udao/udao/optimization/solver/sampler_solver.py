f57eaf0ecb7cc77fc38f7023e7e179cf80c434b6
Julien Fontanarava <julien.fontanarava@gmail.com>
2023-11-30 12:11:46 +0100

    filter_on_constraints as a static method

--- udao/udao/optimization/solver/sampler_solver.py
+++ udao/udao/optimization/solver/sampler_solver.py
@@ -7,7 +7,6 @@ from ..concepts import Constraint, Objective, Variable
 from ..utils.exceptions import NoSolutionError
 from ..utils.moo_utils import Point
 from .base_solver import BaseSolver
-from .utils import filter_on_constraints
 
 
 class SamplerSolver(BaseSolver, ABC):
@@ -62,7 +61,7 @@ class SamplerSolver(BaseSolver, ABC):
         if constraints is None:
             constraints = []
         variable_values = self._get_input(variables)
-        filtered_vars = filter_on_constraints(
+        filtered_vars = self.filter_on_constraints(
             input_parameters=input_parameters,
             input_vars=variable_values,
             constraints=constraints,
@@ -78,3 +77,39 @@ class SamplerSolver(BaseSolver, ABC):
             objs=objective_value[op_ind],
             vars={k: v[op_ind] for k, v in filtered_vars.items()},
         )
+
+    @staticmethod
+    def filter_on_constraints(
+        input_parameters: Optional[Dict[str, Any]],
+        input_vars: Dict[str, np.ndarray],
+        constraints: List[Constraint],
+    ) -> Dict[str, np.ndarray]:
+        """Keep only input variables that don't violate constraints
+
+        Parameters:
+        -----------
+        wl_id : str | None
+            workload id
+        input_vars : np.ndarray
+            input variables
+        constraints : List[Constraint]
+            constraint functions
+        """
+        if not constraints:
+            return input_vars
+
+        available_indices = np.arange(len(next(iter(input_vars.values()))))
+        for constraint in constraints:
+            const_values = constraint.function(
+                input_vars, input_parameters=input_parameters
+            )
+
+            if constraint.type == "<=":
+                compliant_indices = np.where(const_values <= 0)
+            elif constraint.type == ">=":
+                compliant_indices = np.where(const_values >= 0)
+            else:
+                compliant_indices = np.where(const_values == 0)
+            available_indices = np.intersect1d(compliant_indices, available_indices)
+        filtered_vars = {k: v[available_indices] for k, v in input_vars.items()}
+        return filtered_vars
