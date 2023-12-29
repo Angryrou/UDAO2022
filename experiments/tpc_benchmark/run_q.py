951eb012c920599a0dd0782bf26da0288a883414
angryrou <chenghao@cs.umass.edu>
2023-12-29 01:41:34 +0100

    reformat

--- experiments/tpc_benchmark/run_q.py
+++ experiments/tpc_benchmark/run_q.py
@@ -21,67 +21,51 @@ from utils import magic_extract, tensor_dtypes
 logger.setLevel("INFO")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Udao Script with Input Arguments")
+    # fmt: off
     # Data-related arguments
-    parser.add_argument("--benchmark", type=str, default="tpch", help="Benchmark name")
-    parser.add_argument(
-        "--q_type",
-        type=str,
-        default="q_compile",
-        help="graph type, q_compile or q or qs",
-    )
-
+    parser.add_argument("--benchmark", type=str, default="tpch",
+                        help="Benchmark name")
+    parser.add_argument("--q_type", type=str, default="q_compile",
+                        help="graph type, q_compile or q or qs")
     # Embedder parameters
-    parser.add_argument(
-        "--lpe_size",
-        type=int,
-        default=8,
-        help="Laplacian Positional encoding size - for GTN only",
-    )
-    parser.add_argument(
-        "--output_size", type=int, default=32, help="Embedder output size"
-    )
-    parser.add_argument(
-        "--op_groups",
-        nargs="+",
-        default=["type", "cbo", "op_enc"],
-        help="List of operation groups",
-    )
-    parser.add_argument(
-        "--type_embedding_dim", type=int, default=8, help="Type embedding dimension"
-    )
-    parser.add_argument(
-        "--vec_size", type=int, default=16, help="Word2Vec embedding size"
-    )
-    parser.add_argument(
-        "--embedding_normalizer", type=str, default=None, help="Embedding normalizer"
-    )
-
+    parser.add_argument("--lpe_size", type=int, default=8,
+                        help="Laplacian Positional encoding size - for GTN only")
+    parser.add_argument("--output_size", type=int, default=32,
+                        help="Embedder output size")
+    parser.add_argument("--op_groups", nargs="+", default=["type", "cbo", "op_enc"],
+                        help="List of operation groups")
+    parser.add_argument("--type_embedding_dim", type=int, default=8,
+                        help="Type embedding dimension")
+    parser.add_argument("--vec_size", type=int, default=16,
+                        help="Word2Vec embedding size")
+    parser.add_argument("--embedding_normalizer", type=str, default=None,
+                        help="Embedding normalizer")
     # Regressor parameters
-    parser.add_argument(
-        "--n_layers", type=int, default=2, help="Number of layers in the regressor"
-    )
-    parser.add_argument(
-        "--hidden_dim", type=int, default=32, help="Hidden dimension of the regressor"
-    )
-    parser.add_argument("--dropout", type=float, default=0.1, help="Dropout rate")
-
+    parser.add_argument("--n_layers", type=int, default=2,
+                        help="Number of layers in the regressor")
+    parser.add_argument("--hidden_dim", type=int, default=32,
+                        help="Hidden dimension of the regressor")
+    parser.add_argument("--dropout", type=float, default=0.1,
+                        help="Dropout rate")
     # Learning parameters
-    parser.add_argument(
-        "--init_lr", type=float, default=1e-1, help="Initial learning rate"
-    )
-    parser.add_argument(
-        "--min_lr", type=float, default=1e-5, help="Minimum learning rate"
-    )
-    parser.add_argument("--weight_decay", type=float, default=1e-2, help="Weight decay")
-    parser.add_argument(
-        "--epochs", type=int, default=2, help="Number of training epochs"
-    )
-    parser.add_argument("--batch_size", type=int, default=512, help="Batch size")
-    parser.add_argument("--num_workers", type=int, default=15, help="non-debug only")
-
+    parser.add_argument("--init_lr", type=float, default=1e-1,
+                        help="Initial learning rate")
+    parser.add_argument("--min_lr", type=float, default=1e-5,
+                        help="Minimum learning rate")
+    parser.add_argument("--weight_decay", type=float, default=1e-2,
+                        help="Weight decay")
+    parser.add_argument("--epochs", type=int, default=2,
+                        help="Number of training epochs")
+    parser.add_argument("--batch_size", type=int, default=512,
+                        help="Batch size")
+    parser.add_argument("--num_workers", type=int, default=15,
+                        help="non-debug only")
     # Others
-    parser.add_argument("--seed", type=int, default=42, help="Random seed")
-    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="Random seed")
+    parser.add_argument("--debug", action="store_true",
+                        help="Enable debug mode")
+    # fmt: on
 
     args = parser.parse_args()
     device = "gpu" if th.cuda.is_available() else "cpu"
